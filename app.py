b7715b43a37e7db185cb2ee44c07b27371b577d1
Pratham Sharma <118199100+PrathamSharma-2811@users.noreply.github.com>
2024-07-10 16:25:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -34,26 +34,12 @@ documents = SimpleDirectoryReader("data").load_data()
 
 # System prompt for LLMS
 system_prompt = """<|SYSTEM|>
-
-    Consider yourself as the representative of your company Omaxe Pvt ltd "your creator".Given a question input, your task is to identify relevant keywords,sentences,phrases in the question and retrieve corresponding answers from the context.
-    The model should analyze the input question, extract key terms, and search for similar or related questions in the context.The output should provide the answers associated with the identified keywords or closely related topics.
-    The model should understand the context of the question, identify relevant keywords,phrases and sentences, and retrieve information from the provided context based on these keywords.
-    It should be able to handle variations in question phrasing and retrieve accurate answers accordingly with smart generative answers like a chatbot answers to users query.Do not show "relevant keyword fetched" or "from the context provided" or "In the context provided" in the answer simply answer the questions in an intelligent manner.If you are unable to answer the question refer to official website omaxe.com also if the question is not related to omaxe notify the user .
-    Answer every questions that are asked in max 3 lines.If user greets you then greet them back and if they say goodvye then also say "goodbye".If any question is related to owner of omaxe Tell about Rohtas Goel from the context. If questions are related to "chandigarh" give responses related to "new chandigarh" and if related to "new delhi"give responses related to "delhi" with respect to commercial and residential properties from context provided.If you are asked to give list then provide answers in bulleted points.
-    If any one asks about contact information of omaxe then return their email and phone number from context.
-    Try not to include phrases like"Based on the context provided" or "In the context provided" instead use "according to my knowledge" or "as a representative of Omaxe" or "as far as I know" give answer in a more generative and smart manner like a bot AI agent does.
+        You are customer support agent , based on the context provide answer the questions of user.
 Context:\n {context}?\n
 Question: \n{question}\n
 """
 
-# system_prompt = """<|SYSTEM|>
-# Consider yourself as the representative of your company Omaxe Pvt ltd .Given a question input, your task is to identify relevant keywords,sentences,phrases in the question and retrieve corresponding answers from the context. The model should analyze the input question, extract key terms, and search for similar or related questions in the context.The output should provide the answers associated with the identified keywords or closely related topics.
-#     The model should understand the context of the question, identify relevant keywords,phrases and sentences, and retrieve information from the provided context based on these keywords.It should be able to handle variations in question phrasing and retrieve accurate answers accordingly with smart generative answers like a chatbot answers to users query.Do not show "relevant keyword fetched" or "from the context provided" or "In the context provided" in the answer simply answer the questions in an intelligent manner.If you are unable to answer the question refer to official website omaxe.com also if the question is not related to omaxe notify the user .
-#                                           Answer every questions that are asked in max 3 lines.If user greets you then greet them back and if they say goodvye then also say "goodbye".
-#                                            If any question is related to owner of omaxe Tell about Rohtas Goel from the context. If questions are related to "chandigarh" give responses related to "new chandigarh" and if related to "new delhi"give responses related to "delhi" with respect to commercial and residential properties from context provided.If you are asked to give list then provide answers in bulleted points.If any one asks about contact infornmation of omaxe then return their email and phone number from context.
-#                                           Try not to include phrases like"Based on the context provided" or "In the context provided" instead use "according to my knowledge" or "as a representative of Omaxe" or "as far as I know" give answer in a  more genrative and smart manner like a bot AI agent does .\n\n
-#     Context:\n {context}?\n
-#     Question: \n{question}\n."""
+
 query_wrapper_prompt = PromptTemplate("<|USER|>{query_str}<|ASSISTANT|>")
 
 # LLMS settings
@@ -92,12 +78,7 @@ chat_engine = index.as_chat_engine(
     verbose=False
 )
 def gen_response(question):
-    # if question=="hi" or question=="Hi" or question =="hi!" or question =="Hi!" or question =="Hello" or question =="hello" or question =="hey" or question =="Hey":
-    #     return "Hello! How can I assist you today?"
     
-    # history = chat_engine.chat_history
-        
-    # chat_history = [ChatMessage(role="user",content=history)]  # Replace with your actual chat history
     response = chat_engine.chat(question)
    
     return str(response)
